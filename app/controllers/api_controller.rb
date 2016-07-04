dcf20500dd09f49be47807bc4c998228f3745b55
Mayur Patel <mpatel@uscamwebd61.boston.zipcar.com>
2016-07-04 09:46:51 -0400

    get distance

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,14 @@
 class ApiController < ApplicationController
 
- include HTTParty
 
 	def process_ride
        	scode=get_pnr_status()
         puts scode 
-        get_station_coordinates(scode)
+        stn_cord=get_station_coordinates(scode)
+        user_cord=get_user_cordinates()
+        dist=get_distance_and_time(user_cord[0],user_cord[1],stn_cord[0],stn_cord[1])
+        puts dist
+        
 	end
 
         def uber_ride
@@ -27,7 +30,7 @@ p q
 
 #Replace hardcoded PNR by pnr provided in request
 	def get_pnr_status(pnr="4220338987")
-	 s = Net::HTTP.get_response(URI.parse("http://api.railwayapi.com/pnr_status/pnr/#{pnr}/apikey/cbgmi8296/")).body
+	 s = Net::HTTP.get_response(URI.parse("http://api.railwayapi.com/pnr_status/pnr/#{pnr}/apikey/yjfpq3086/")).body
      q = JSON.parse(s)
      @scode=q["from_station"]["code"]	
      return "#{@scode}"
@@ -36,21 +39,24 @@ p q
 
 #Replcae hardcoded ADH staion with source staion code from get_pnr_status
 	def get_station_coordinates(scode)
-		s = Net::HTTP.get_response(URI.parse("http://api.railwayapi.com/code_to_name/code/#{scode}/apikey/cbgmi8296/")).body
+		s = Net::HTTP.get_response(URI.parse("http://api.railwayapi.com/code_to_name/code/#{scode}/apikey/yjfpq3086/")).body
           @q = JSON.parse(s)["stations"]
           @q.each do |stncode|
     		if stncode["code"] .eql? "#{scode}"
-    			puts stncode["lat"]
-    			puts stncode["lng"]
                 	return stncode["lat"],stncode["lng"]
     		end
 	  end
 	end
 
-	def get_user_cordinates
+	def get_user_cordinates(user_lat="19.0728300",user_lng="72.8826100")
+        	return "#{user_lat}","#{user_lng}"
 	end
 
-	def get_distance_and_time
+	def get_distance_and_time(user_lat,user_lng,stn_lat,stn_lng)
+        	 s = Net::HTTP.get_response(URI.parse("https://api.mapmyindia.com/v3?fun=star_dists&lic_key=5h9mbi7h2jvdszbvzcir1c4llwohp9rt&center=#{user_lat},#{user_lng}&pts=#{stn_lat},#{stn_lng}&rtype=0&vtype=1&avoids=0110")).body
+
+        	q = JSON.parse(s)
+		return q[0]["duration"]
 	end
 
 	def db_store
