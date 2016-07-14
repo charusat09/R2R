42234e0ab5957383b211ddb32cad25a4234bd404
Mayur Patel <mpatel@uscamwebd61.boston.zipcar.com>
2016-07-14 07:35:03 -0400

    adds new feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
         match 'api/process_ride' => 'api#process_ride', :via => [:get,:post]
 	match 'api/process_ride' => 'api#process_ride', :via => [:get,:post]
         match 'api/address' => 'api#address', :via => [:get,:post]
+	match 'api/confirmation' => 'api#confirmation', :via => [:get]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
